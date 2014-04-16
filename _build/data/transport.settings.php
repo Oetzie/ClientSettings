7719359a5393e54228ee49895045f752404c34e0
Oene Tjeerd de Bruin <oenetjeerddebruin@oetzienlmacbook.lan.nhl.nl>
2014-04-16 10:29:42 +0200

    Version update

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -4,7 +4,7 @@
 
 	$settings[0] = $modx->newObject('modSystemSetting');
 	$settings[0]->fromArray(array(
-		'key' 		=> PKG_NAME_LOWER.'.admin_groups',
+		'key' 		=> PKG_NAME_LOWER.'_admin_groups',
 		'value' 	=> 'Administrator',
 		'xtype' 	=> 'textfield',
 		'namespace' => PKG_NAME_LOWER,
